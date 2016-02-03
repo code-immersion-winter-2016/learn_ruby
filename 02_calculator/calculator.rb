154351cdc0a44620924d0f64460a6738d66a3316
Your Name <you@example.com>
2016-02-03 15:16:38 -0500

    adds 02_calculator home work

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,12 @@
+def add (num_1, num_2 )
+  return num_1 + num_2
+end
+
+def subtract (num_1, num_2 )
+  return num_1 - num_2
+end
+
+def sum (num = 0)  #(num_2)
+   num = 0
+  #  num_2 = {|x,y| (x + y) }
+end
